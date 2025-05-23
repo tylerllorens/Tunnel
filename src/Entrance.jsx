e5fb8fbae4e778c22c8d4f4ceeb63191d924ffad
Tyler Llorens <tylerllore@gmail.com>
2025-05-23 11:01:26 -0400

    finished code

--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,12 @@
-/** Users can enter their name to receive a token from the API. */
+import { useAuth } from "./AuthContext";
+
 export default function Entrance() {
-  // TODO: call signup when form is submitted
+  const { signup } = useAuth();
+
+  const speakName = (formData) => {
+    const name = formData.get("name");
+    signup({ username: name });
+  };
 
   return (
     <>
@@ -15,7 +21,7 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form action={speakName}>
         <label>
           Name
           <input name="name" />
